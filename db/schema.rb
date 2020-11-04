e1e001aef4a3dfc65ee854b1d3aea14879b359ea
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2020-11-04 13:39:43 -0700

    peel books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_182732) do
+ActiveRecord::Schema.define(version: 2020_10_30_205050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -87,6 +87,14 @@ ActiveRecord::Schema.define(version: 2020_08_31_182732) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["owner_id"], name: "index_communities_on_owner_id"
+  end
+
+  create_table "digitization_books", force: :cascade do |t|
+    t.integer "peel_id"
+    t.integer "run"
+    t.integer "part_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "draft_items", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
@@ -211,7 +219,6 @@ ActiveRecord::Schema.define(version: 2020_08_31_182732) do
     t.json "embargo_history", array: true
     t.json "is_version_of", array: true
     t.json "member_of_paths", null: false, array: true
-    t.json "subject", array: true
     t.json "creators", array: true
     t.json "contributors", array: true
     t.string "created"
@@ -229,7 +236,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_182732) do
     t.string "aasm_state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.json "deconcatenated_subject", array: true
+    t.json "subject", array: true
     t.index ["logo_id"], name: "index_items_on_logo_id"
     t.index ["owner_id"], name: "index_items_on_owner_id"
   end
@@ -274,7 +281,6 @@ ActiveRecord::Schema.define(version: 2020_08_31_182732) do
     t.json "embargo_history", array: true
     t.json "is_version_of", array: true
     t.json "member_of_paths", null: false, array: true
-    t.json "subject", array: true
     t.text "abstract"
     t.string "language"
     t.datetime "date_accepted"
@@ -294,7 +300,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_182732) do
     t.string "aasm_state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.json "deconcatenated_subject", array: true
+    t.json "subject", array: true
     t.index ["logo_id"], name: "index_theses_on_logo_id"
     t.index ["owner_id"], name: "index_theses_on_owner_id"
   end
